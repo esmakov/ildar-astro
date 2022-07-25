1a22e23bff093dbb1b537a09d00103b5b758cc31
esmakov <edwardsmakov@gmail.com>
2022-07-25 10:02:38 -0400

    New message for the combined commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-
+import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()]
+  integrations: [preact(), tailwind(), image()]
 });
