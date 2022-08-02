032dc8258bc88a2c904ecb86b48855b923cd2280
esmakov <edwardsmakov@gmail.com>
2022-08-02 08:35:07 -0400

    Fix: partytown import in astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
