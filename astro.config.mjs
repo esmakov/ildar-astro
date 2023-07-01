baaec60877d42d0de4037f1240dc7a2bfe260feb
esmakov <edwardsmakov@gmail.com>
2023-07-01 15:41:05 -0400

    Remove preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
-import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    preact(),
     tailwind(),
     partytown({
       // Adds dataLayer.push as a forwarding-event.
