f7edc8ab868c47d4f7dc607f137e4791268ebc61
esmakov <edwardsmakov@gmail.com>
2023-08-13 18:36:10 -0400

    Center video titles

---
+++ prettier.config.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  plugins: [
+    require.resolve("prettier-plugin-astro"),
+    require.resolve("prettier-plugin-tailwindcss"),
+  ],
+  pluginSearchDirs: false,
+  // overrides: [
+  //   {
+  //     files: "*.astro",
+  //     options: {
+  //       parser: "astro",
+  //     },
+  //   },
+  // ],
+};
